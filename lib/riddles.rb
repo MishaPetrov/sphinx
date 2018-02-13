7936a9d2039a0c37f4dd80e5a0c74b9babf50e2b
Misha Petrov and Russell Hofvendahl <misha@>
2018-02-13 10:32:14 -0800

    pass select_random test

--- lib/riddles.rb
+++ lib/riddles.rb
@@ -8,7 +8,7 @@ class Riddles
       "I'm white, and used for cutting and grinding. When I'm damaged, humans usually remove me or fill me. For most animals I am a useful tool. What am I?" => "tooth"}
   end
 
-  # def select_random
-  #   @current_riddle = @riddle_pool.keys.sample
-  # end
+  def select_random
+    @current_riddle = @riddle_pool.keys.sample
+  end
 end
