452d7c1b6b2b4b04b08c7a8d54a91773a0986ef2
Misha Petrov and Russell Hofvendahl <misha@>
2018-02-13 10:41:07 -0800

    add test for evaluate_answer

--- spec/riddles_spec.rb
+++ spec/riddles_spec.rb
@@ -3,10 +3,19 @@ require('riddles')
 require('pry')
 
 describe('Riddles') do
-  describe('#select_random')
+  describe('#select_random') do
     it("selects random riddle from the pool") do
       riddles = Riddles.new()
       riddles.select_random
       expect(riddles.riddle_pool.key?(riddles.current_riddle)).to(eq(true))
     end
+  end
+  describe('#evaluate_answer') do
+    it("evaluates if user response matches riddle answer") do
+      riddles = Riddles.new()
+      riddles.select_random
+      correct_answer = riddles.riddle_pool[:current_riddle]
+      expect(riddles.evaluate_answer(correct_answer)).to(eq(true))
+    end
+  end
 end
